c307e76fe53edb89f2119273a709b59331809428
Yurii Bliuchak <1957659+bliuchak@users.noreply.github.com>
2024-04-21 11:45:26 +0200

    feat(nvim): remap some keymaps (prolly tmux integration)
    - honesly don't remember whats is this :|

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+local opts = { noremap = true, silent = true }
+map("i", "<C-h>", "<Left>", opts)
+map("i", "<C-j>", "<Down>", opts)
+map("i", "<C-k>", "<Up>", opts)
+map("i", "<C-l>", "<Right>", opts)
